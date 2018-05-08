ad50eb274d784db5f4bbfe09ce39f0b32201e57b
余争飞 <feiyu22@vip.qq.com>
2018-05-08 11:13:33 +0800

    bug修改

--- src/main/java/com/whyixiu/erp/service/SysUserService.java
+++ src/main/java/com/whyixiu/erp/service/SysUserService.java
@@ -1,10 +1,8 @@
 package com.whyixiu.erp.service;
 
-import org.springframework.stereotype.Service;
 
 import com.whyixiu.erp.model.SysUser;
 
-@Service
 public interface SysUserService {
 
 	SysUser getByName(String name);
